99f40e76520341e6a12a2b14db546315936e749f
Tahsin <alahitahsin@gmail.com>
2024-10-30 20:11:24 +0600

    fixed loading issue of allBookLoader

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,16 +1,22 @@
 export async function allBookLoader() {
+  let cachedStorage = sessionStorage.getItem("allBooks");
+
+  if (cachedStorage) {
+    return JSON.parse(cachedStorage);
+  }
+
   const res = await fetch("https://openlibrary.org/trending/yearly.json");
   const data = await res.json();
+  cachedStorage = data.works.slice(0, 15);
+  sessionStorage.setItem("allBooks", JSON.stringify(cachedStorage));
 
-  const returnData = data.works.slice(0, 15);
-
-  return returnData;
+  return cachedStorage;
 }
 
 function HomePage() {
   return (
     <main>
-      <div>Books</div>
+      <div>book</div>
     </main>
   );
 }
