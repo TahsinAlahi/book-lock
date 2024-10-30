4bde5594efc4e81dd069aeb155307a22f210c366
Tahsin <alahitahsin@gmail.com>
2024-10-31 02:50:32 +0600

    made BookCard

--- src/Components/BookCard.jsx
+++ src/Components/BookCard.jsx
@@ -1,15 +1,40 @@
-import { useBooks } from "../Contexts/BookContext";
+import { useState } from "react";
+import Loader from "./Loader";
 
-function BookCard() {
-  const { allBooks } = useBooks();
+function BookCard({ book }) {
+  const [isImgLoaded, setIsImgLoaded] = useState(false);
+  const coverImg = `https://covers.openlibrary.org/b/olid/${book.cover_edition_key}-L.jpg`;
 
-  const book = allBooks[0];
+  return (
+    <div className="grid lg:grid-cols-2 gap-5 p-6 border-2 border-black/10 rounded-lg shadow-lg">
+      <div className="w-full h-fit">
+        {!isImgLoaded && <Loader />}
+        <img
+          src={coverImg}
+          alt={book.title}
+          className={`${isImgLoaded ? "block" : "hidden"} rounded-lg`}
+          onLoad={() => setIsImgLoaded(true)}
+        />
+      </div>
 
-  const isbn = book.availability.isbn;
-
-  const coverImg = `https://covers.openlibrary.org/b/isbn/${isbn}-L.jpg`;
-
-  return <div>{/* <img src={"coverImg"} alt={book.title} /> */}</div>;
+      <div className=" flex items-start justify-around flex-col ">
+        <div>
+          <h2 className="lg:text-2xl font-bold text-right lg:mb-1">
+            {book.title}
+          </h2>
+          <p className="font-medium text-black/40">By: {book.author_name[0]}</p>
+        </div>
+        <div className="flex flex-col w-full gap-3">
+          <button className="bg-lime-500 w-full py-2 border-2 border-gray-300 rounded-lg text-white font-semibold">
+            Details
+          </button>
+          <button className="bg-cyan-500 w-full py-2 border-2 border-gray-300 rounded-lg text-white font-semibold">
+            Add To Wishlist
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BookCard;
--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -1,5 +1,11 @@
 function BookList() {
-  return <div>BookList</div>;
+  return (
+    <div className="lg:mt-10">
+      <h1 className="text-4xl font-bold text-center lg:mb-10">Books</h1>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8"></div>
+    </div>
+  );
 }
 
 export default BookList;
